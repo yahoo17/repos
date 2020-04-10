c8d341244d3e98532ef4ef6afc4c16ca94c2aa04
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-04-10 10:31:51 +0800

    1

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,74 @@
+﻿// ConsoleApplication6.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+void quickSort(int arr[], int left, int right) {
+
+    int i = left, j = right;
+
+    int tmp;
+
+    int pivot = arr[(left + right) / 2];
+
+
+
+    /* partition */
+
+    while (i <= j) {
+
+        while (arr[i] < pivot)
+
+            i++;
+
+        while (arr[j] > pivot)
+
+            j--;
+
+        if (i <= j) {
+
+            tmp = arr[i];
+
+            arr[i] = arr[j];
+
+            arr[j] = tmp;
+
+            i++;
+
+            j--;
+
+        }
+
+    };
+
+
+
+    /* recursion */
+
+    if (left < j)
+
+        quickSort(arr, left, j);
+
+    if (i < right)
+
+        quickSort(arr, i, right);
+
+}
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int array[] = { 10,80,30,20,60 };
+    quickSort(array, 0, 4);
+    for (auto x : array)
+        std::cout << x << " ";
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
