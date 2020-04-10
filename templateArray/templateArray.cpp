c950594d236c50a731c73f352e15ca25a9c78693
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-04-10 18:20:03 +0800

    1

--- templateArray/templateArray.cpp
+++ templateArray/templateArray.cpp
@@ -1,20 +1,50 @@
-﻿// templateArray.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include <iomanip>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//
+vector<double> result;
+void printPercent(int score, vector<int> scoreVector,int size)
+{
+   
+    auto it = find(scoreVector.begin(), scoreVector.end(), score);
+    
+    if (it == scoreVector.end())
+        cout << fixed << setprecision(6) << 100 << endl;
+    else
+    {
+        int index = it - scoreVector.begin();
+        double percent = index *100.0/ size;
+        result.push_back(percent);
 
+        
+    }
+        
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    int size;
+    cin >> size;
+    vector<int> score;
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+        cin >> temp;
+        score.push_back(temp);
+    }
+    vector<int> sortScore = score;
+    sort(sortScore.begin(), sortScore.end());
+    int num;
+    cin >> num;
+    num=num > size ? size : num;
+    for (int i = 0; i < num; i++)
+    {
+        cin >> temp;
+        printPercent(score[temp-1], sortScore,size);
+    }
+    for (auto x : result)
+    {
+        cout << fixed << setprecision(6) << x << endl;
+    }
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
