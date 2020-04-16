0f26b849164f753261b53944fa34bd459e93c367
yahoo17 <55846951+yahoo17@users.noreply.github.com>
2020-04-16 15:43:53 +0800

    !

---
+++ arraytemplate2/arraytemplate2.cpp
@@ -0,0 +1,82 @@
+﻿//学号201830020446 严灏 序号1
+#include<iostream>
+#include<iomanip>
+using namespace std;
+template<class T>
+void Swap(T& t1, T& t2)
+{
+    T tmp;
+    tmp = x;
+    x = y;
+    y = tmp;
+}
+
+template<class T>
+class Array
+{
+private:
+    T* val;
+    int size;
+public:
+    Array(int s = 10);
+    ~Array();
+    T getsum();
+    T& operator[](int sub);
+    Array& operator=(const Array& x);
+};
+
+template<class T>
+Array<T>::Array(int s) : size(s)
+{
+    val = new T[s];
+}
+
+template<class T>
+Array<T>::~Array()
+{
+    delete[] val;
+}
+
+template<class T>
+T Array<T>::getsum()
+{
+    T res = 0;
+    for (int i = 0; i < size; i++) res += val[i];
+    return res;
+}
+
+template<class T>
+T& Array<T>::operator[](int sub)
+{
+    return val[sub];
+}
+
+//重载=运算符
+template<class T>
+Array<T>& Array<T>::operator=(const Array<T>& x)
+{
+    size = x.size;
+    delete[] val;
+    val = new T[size];
+    for (int i = 0; i < size; i++) val[i] = x.val[i];
+    return *this;
+}
+
+int main()
+{
+    Array<int> a;
+    a = Array<int>(100);
+    int i, n, m;
+    cin >> n >> m;
+    Array<int> a1(n), a2(m);
+
+    for (i = 0; i < n; i++) cin >> a1[i];
+    for (i = 0; i < m; i++) cin >> a2[i];
+
+    Swap(a1, a2);
+    for (i = 0; i < m; i++) cout << a1[i] << ' ';
+    cout << endl;
+    for (i = 0; i < n; i++) cout << a2[i] << ' ';
+
+}
+
---
+++ void/void.cpp
@@ -0,0 +1,89 @@
+﻿// void.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<iomanip>
+using namespace std;
+//class A
+//{
+//public:
+//    static void (*__malloc_alloc_oom_handler)();
+//public:
+//    static void *(set_malloc_hander(void (*f)()))()
+//    {
+//        void (*old)() = __malloc_alloc_oom_handler;
+//        __malloc_alloc_oom_handler = f;
+//        return (old);
+//    }
+//};
+int main()
+{
+    /*char ch[] = "hello h222\n hello2";
+    cout << ch << endl << endl;
+    cout << &ch << endl;
+    cout << *ch << endl;
+    char* p = &ch[0];
+    cout << static_cast<void*>(p) << endl << endl;*/
+
+    /*cout << p << endl << endl;*/
+   /* cout.put('A');
+    cout.put(65);*/
+    //cout << "Enter a list intergers:" << endl;
+    //int currVal = 0;
+    //int  val = 0;
+    //int cnt = 0;
+    //if (cin >> currVal)
+    //{
+    //    cnt = 1;
+    //    //
+    //    while (cin >> val)
+    //    {
+    //        if (currVal == val)
+    //        {
+    //            ++cnt;
+    //        }
+    //        else
+    //        {
+    //            cout << currVal << " occurs" << cnt << "  times" << endl;
+    //            currVal = val;
+    //            cnt = 1;
+    //        }
+    //       
+    //    }
+    //    /*cout << currVal << " occurs" << cnt << "times. " << endl;*/
+    //}
+    //return 0;
+    //const int SIZE = 100;
+    //char buffer[SIZE];
+    //cin.read(buffer, 20);
+    //
+    //cout.write(buffer, cin.gcount());
+    /*cout << hex << 16;
+    cout << dec<<16;*/
+    //cout << setbase(16) << 100;
+    //cout << 100;
+    /*char sentence[10];
+    int val = 5;
+    while (cin>>sentence)
+    {
+        cout.width(val++);
+        cout << sentence;
+        cin.width(5);
+
+    }*/
+    cout<<cin.rdstate()<<endl;
+    
+    cout << cin.good()<<endl;
+    cout << cin.bad() << endl;
+    cout << cin.fail() << endl;
+    cout << cin.eof()<<endl;
+
+    int a;
+    cin >> a;
+    cout << cin.rdstate() << endl;
+    cout << cin.good() << endl;
+    cout << cin.bad() << endl;
+    cout << cin.fail() << endl;
+    cout << cin.eof() << endl;
+}
+
